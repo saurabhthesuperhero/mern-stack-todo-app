94b4859ca5e52b8707a56b185adf17682e77719e
saurabhthesuperhero <2021Momisgoid1234$>
2022-09-15 18:59:02 +0530

    inrprocess

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // server.js
+require("dotenv").config(); //added
 const express = require("express");
-
+const connectDB = require("./config/db"); //added
 const app = express();
+
+
+// routes
+const todo = require("./routes/todo"); // added
+
+// connect database
+connectDB();//added
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/todo", todo); // added
 
 // setting up port
 const PORT = process.env.PORT || 8000;
